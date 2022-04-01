5b348c813e5a5672d9feaaefeeeca919888efa7b
eselivanova <ekaterina.selivanova@dataart.com>
2022-04-01 14:03:25 +0300

    JFIES-15. Wrote unit tests for user, lesson and booking services.

--- src/main/java/com/dataart/dancestudio/model/dto/view/UserViewDto.java
+++ src/main/java/com/dataart/dancestudio/model/dto/view/UserViewDto.java
@@ -3,10 +3,8 @@ package com.dataart.dancestudio.model.dto.view;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 @EqualsAndHashCode
 @AllArgsConstructor
 public class UserViewDto {
